f1c183c11cf5da3da3b4b6cb229f1497d1baad64
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-09-21 17:02:06 -0700

    Task 6:

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for x in range(10):
+    for y in range(10):
+        if x  < y:
+            print(f"{x}{y}", end = "")
+            if ((x * 10) +  y) != 89:
+                print(", ", end = "")
+            else:
+                print("\n")
+                break
