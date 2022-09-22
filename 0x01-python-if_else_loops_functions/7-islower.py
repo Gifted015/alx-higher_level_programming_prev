58fb49df31cee2d4c56674368a0bada7a01ceb1a
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-09-21 19:11:18 -0700

    Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
+    c = ord(c)
     for x in range(97, 123):
-        if c == chr(x):
+        if c == x:
             return True
     else:
         return False
