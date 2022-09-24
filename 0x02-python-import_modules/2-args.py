ccf16fd836c46591fb432d9dd3725a2dd69aef49
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-09-24 08:33:20 -0700

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    a = len(argv) - 1
+    if a == 1:
+        print("{} argument".format(a), end="")
+    else:
+        print("{} arguments".format(a), end="")
+
+    if (a) == 0:
+        print(".")
+    else:
+        print(":")
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
