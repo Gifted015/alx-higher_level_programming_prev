75c2e77ad7576849427de79f146d2e5c59fcefe8
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-09-27 10:09:51 -0700

    Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list != None:
+    try:
         for x in range(len(my_list) - 1, -1, -1):
             if type(my_list[x]) == int:
                 print("{:d}".format(my_list[x]))
+    except TypeError:
+        pass
