f40853dc540532a99f53c5a7986cd7f916a5b87f
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-09-28 20:59:10 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(list(a_dictionary)) == 0:
         return None
     biggest = 0
     for curr_key in a_dictionary.keys():
