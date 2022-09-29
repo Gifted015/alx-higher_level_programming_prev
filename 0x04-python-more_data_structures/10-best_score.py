435e1a247bbf44a1ec450e6d69d0270050d010b5
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-09-28 20:16:49 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,11 +5,15 @@ def best_score(a_dictionary):
     except TypeError:
         return None
 
+    x = 0
     for x in range(len(key_list) - 1):
         next_key = key_list[x + 1]
         curr_key = key_list[x]
         biggest = None
         if a_dictionary[curr_key] > a_dictionary[next_key]:
             biggest = curr_key
+
+    if x == 0:
+        return None
 
     return biggest
