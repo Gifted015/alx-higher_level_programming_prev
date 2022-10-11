4b8001bc209cccb424cf5ab535de69ef026f9e3b
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-10-11 01:41:46 -0700

    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        temp = int(value)
+        print("{:d}".format(temp))
+        return True
+    except ValueError:
+        return False
