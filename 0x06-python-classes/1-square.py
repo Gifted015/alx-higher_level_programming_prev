09aa7098429af14791088773d45f8b0c4eb0c4f9
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-10-11 15:57:42 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains a class Square"""
+
+
+class Square:
+    """An empty class that defines a square"""
+
+    def __init__(self, size):
+        """Instantation of a private attribute 'size'
+
+        Args:
+           size: the size of a square
+
+        """
+        self.__size = size
