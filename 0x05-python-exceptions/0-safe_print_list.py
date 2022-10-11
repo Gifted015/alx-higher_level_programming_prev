191e441f83721654b6efb31a6734983a26ee75fa
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-10-11 01:35:31 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     length = 0
     for y in range(0, x):
         try:
-            print(my_list[y])
+            print(my_list[y], end='')
             length += 1
         except IndexError:
             break
+    print('')
     return length
