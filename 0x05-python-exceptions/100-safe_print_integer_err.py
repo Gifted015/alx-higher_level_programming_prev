31bd5d87e598870e7ad1a61eebe94c3c92f98f99
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-10-13 14:23:28 -0700

    Task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except BaseException as err:
-        error = f"Exception: {err}"
+        error = f"Exception: {err}\n"
         os.write(2, str.encode(error))
         return False
