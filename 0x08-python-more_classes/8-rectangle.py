58fa464caa81e7abe9960d28743c52bae37ce721
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-10-20 11:42:52 +0100

    Task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -84,13 +84,10 @@ class Rectangle:
 
     def bigger_or_equal(rect_1, rect_2):
         if (type(rect_1) != Rectangle):
-            print(type(rect_1))
             raise TypeError("rect_1 must be an instance of Rectangle")
         if (type(rect_2) != Rectangle):
-            print(type(rect_2))
             raise TypeError("rect_2 must be an instance of Rectangle")
         if (rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area()):
             return rect_1
         else:
             return rect_2
-        
